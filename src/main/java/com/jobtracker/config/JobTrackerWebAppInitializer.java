1ed5337bcc698781dbfa7b10e28aa1fc4a0aa037
Justin <justin.n.4rws@gmail.com>
2019-09-15 16:44:59 -0400

    Serve a single HTML file
    Basic Java configuration for serving a single HTML file

---
+++ src/main/java/com/jobtracker/config/JobTrackerWebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.jobtracker.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class JobTrackerWebAppInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
+}
